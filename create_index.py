415efec8f7714148b19116ee6395456581f68702
Du'An Lightfoot <29664932+labeveryday@users.noreply.github.com>
2024-04-01 20:54:25 -0400

    Added logging

--- create_index.py
+++ create_index.py
@@ -1,12 +1,40 @@
-import boto3
+import logging
 import os
+import boto3
 from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth
 
 
-OPENSEARCH_ENDPOINT = os.environ["opensearch_host"]
-VECTOR_INDEX_NAME = os.environ["vector_index_name"]
-VECTOR_FIELD_NAME = os.environ["vector_field_name"]
-REGION = os.environ["aws_region"]
+
+# Set up logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+# Create a file handler
+log_file = 'app.log'
+file_handler = logging.FileHandler(log_file)
+file_handler.setLevel(logging.INFO)
+
+# Create a console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# Create a formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+try:
+    OPENSEARCH_ENDPOINT = os.environ["opensearch_host"]
+    VECTOR_INDEX_NAME = os.environ["vector_index_name"]
+    VECTOR_FIELD_NAME = os.environ["vector_field_name"]
+    REGION = os.environ["aws_region"]
+except Exception as e:
+    logger.error(f"Unable to import environmental variable: {e}")
 
 credentials = boto3.Session().get_credentials()
 service = 'aoss'
@@ -44,8 +72,12 @@ def create_index():
                 }
             },
         }
-        res = ops_client.indices.create(index=VECTOR_INDEX_NAME, body=settings, ignore=[400])
-        print(res)
+        try:
+            res = ops_client.indices.create(index=VECTOR_INDEX_NAME, body=settings, ignore=[400])
+            logger.info(f'Index creation response: {res}')
+        except Exception as e:
+            logger.error(f'Error creating index: {e}')
 
 
-create_index()
+if __name__ == "__main__":
+    create_index()
